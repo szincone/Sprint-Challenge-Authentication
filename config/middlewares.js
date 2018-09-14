f88ec9e958eae1321f3a2e2c677030f8063ec6be
szincone <sawyerzincone@gmail.com>
2018-09-14 09:26:23 -0600

    finished register endpoint

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,26 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const jwtKey = require('../_secrets/keys').jwtKey;
+const jwtKey = require("../_secrets/keys").jwtKey;
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken,
 };
-
+// generate token function
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "1h",
+    jwtid: "12345",
+  };
+  return jwt.sign(payload, jwtKey, options);
+}
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+  const token = req.get("Authorization");
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
@@ -21,7 +32,7 @@ function authenticate(req, res, next) {
     });
   } else {
     return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
+      error: "No token provided, must be set on the Authorization Header",
     });
   }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,35 @@
-const axios = require('axios');
-
-const { authenticate } = require('./middlewares');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig.js");
+const { authenticate, generateToken } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10);
+  creds.password = hash;
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      db("users")
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token });
+        })
+        .catch(err => {
+          err.code = 500;
+          next(err);
+        });
+    });
 }
 
 function login(req, res) {
@@ -19,12 +39,12 @@ function login(req, res) {
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten",
     )
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
