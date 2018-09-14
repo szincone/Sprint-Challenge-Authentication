7db001b7f94beb2837d89a2d9e7ab590b6781867
szincone <sawyerzincone@gmail.com>
2018-09-14 09:18:54 -0600

    basic api up and running

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { server } = require('./server.js');
+const { server } = require("./server.js");
 
-const port = process.env.PORT || 3300;
+server.get("/", (req, res) => {
+  res.status(200).send("Working");
+});
+const port = process.env.PORT || 7500;
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
 });
